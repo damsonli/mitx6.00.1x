3bd47738b1680a6267b5dcb8839b62ccbd47c3e3
Yi Li <yil3@cisco.com>
2018-05-28 00:28:36 -0400

    after week 2

---
+++ Problem Set 2/guess_my_number.py
@@ -0,0 +1,61 @@
+'''
+In this problem, you'll create a program that guesses a secret number!
+
+The program works as follows: you (the user) thinks of an integer between 0 (inclusive) and 100 (not inclusive). The computer makes guesses, and you give it input - is its guess too high or too low? Using bisection search, the computer will guess the user's secret number!
+
+Here is a transcript of an example session:
+
+Please think of a number between 0 and 100!
+Is your secret number 50?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. l
+Is your secret number 75?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. l
+Is your secret number 87?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. h
+Is your secret number 81?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. l
+Is your secret number 84?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. h
+Is your secret number 82?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. l
+Is your secret number 83?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. c
+Game over. Your secret number was: 83
+
+Note: your program should use input to obtain the user's input! Be sure to handle the case when the user's input is not one of h, l, or c.
+
+When the user enters something invalid, you should print out a message to the user explaining you did not understand their input. Then, you should re-ask the question, and prompt again for input. For example:
+
+Is your secret number 91?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. y
+Sorry, I did not understand your input.
+Is your secret number 91?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. c
+'''
+print("Please think of a number between 0 and 100!")
+
+# At the start the highest the number could be is 100 and the lowest is 0.
+hi = 100
+lo = 0
+guessed = False
+
+# Loop until we guess it correctly
+while not guessed:
+    # Bisection search: guess the midpoint between our current high and low guesses
+    guess = (hi + lo)//2
+    print("Is your secret number " + str(guess)+ "?")
+    user_inp = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+
+    if user_inp == 'c':
+        # We got it right!
+        guessed = True
+    elif user_inp == 'h':
+        # Guess was too high. So make the current guess the highest possible guess.
+        hi = guess
+    elif user_inp == 'l':
+        # Guess was too low. So make the current guess the lowest possible guess.
+        lo = guess
+    else:
+        print("Sorry, I did not understand your input.")
+
+print('Game over. Your secret number was: ' + str(guess))
